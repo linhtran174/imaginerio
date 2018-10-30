2c1b1883264a23ab79ccfb5e3dd24546461ec9d7
Linh Tran <linhtran17495@gmail.com>
2018-10-30 17:44:12 +0700

    correctly overlay maps on mapCollector page

--- src/collector/map.js
+++ src/collector/map.js
@@ -44,11 +44,14 @@ $("#mapImage").onchange = (e)=>{
 
         var t = linearTransform(featurePoints, matchPoints);
         var r = (new FileReader())
+        var topLeft = t([0,0]);
+        var bottomRight =  t([$("#mapImg").height, $("#mapImg").width]);
         r.onload = ()=>{
-            // L.imageOverlay(
-            //     r.result,
-            //     [ t([0,0]), t([$("#mapImg").width, $("#mapImg").height])]
-            // ).addTo(leafletMap);
+            L.imageOverlay(
+                r.result,
+                [topLeft, bottomRight],
+                {opacity: 0.7}
+            ).addTo(leafletMap);
 
             // L.imageOverlay(
             //     r.result,
@@ -73,26 +76,35 @@ $("#mapImage").onchange = (e)=>{
 }
 
 function linearTransform(featurePoints, matchPoints){
-    var offset = [
-        matchPoints[0][0] - featurePoints[0][0],
-        matchPoints[0][1] - featurePoints[0][1]
-    ];
-    var o = function (arr){  
-        return [arr[0]+offset[0], arr[1]+offset[1]];
-    };
+    featurePoints = featurePoints.map(p=>[p[1],p[0]]);
+    // console.log()
+    // var offset = [
+    //     matchPoints[0][0] - featurePoints[0][0],
+    //     matchPoints[0][1] - featurePoints[0][1]
+    // ];
+    // var o = function (arr){  
+    //     return [arr[0]+offset[0], arr[1]+offset[1]];
+    // };
+    console.log("matchPoints: ", matchPoints, "featurePoints", featurePoints);
 
-    var a1 = o(featurePoints[0]);
-    var b1 = o(featurePoints[1]);
+    var a1 = featurePoints[0];
+    var b1 = featurePoints[1];
     var a2 = matchPoints[0];
     var b2 = matchPoints[1];
     var scale = [
-        (a1[0] - b1[0]) / (a2[0] - b2[0])
-    ]
-    var scale = function (arr){
-        
+        (a2[0] - b2[0])/ (a1[0] - b1[0]),
+        (a2[1] - b2[1])/ (a1[1] - b1[1]) 
+    ];
+    console.log("scale: ", scale);
+    var s = function (arr){
+        return [
+            a2[0] + (arr[0] - a1[0]) * scale[0],
+            a2[1] + (arr[1] - a1[1]) * scale[1]
+        ];
     }
 
     return function(point){
+        return s(point);
     };
 }
 
@@ -102,8 +114,10 @@ $("#panzoomSelectPointToggle").onclick = (e)=>{
 
 document.onkeydown = (e)=>{
     if (e.code == "Space"){
-        e.stopPropagation();
-        e.preventDefault();
+        if(e.target == $("#uploadMap")){
+            e.stopPropagation();
+            e.preventDefault();
+        }
         selectFeaturePoint.toggleZoomSelect("keyPress")   
     }
     if (e.code == "Escape"){
